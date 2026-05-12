456d8f96a0356c9fa04cf1d443cd38d8370e8aac
Gabriel Erick <gabrielmmve13@gmail.com>
2026-05-12 15:59:39 -0300

    exibindo as músicas de um álbum

--- aplicando-poo/ScreenSound/Model/Album.cs
+++ aplicando-poo/ScreenSound/Model/Album.cs
@@ -4,12 +4,23 @@ namespace Model
     {
         private List<Musica> musicas = new List<Musica>();
         public string Nome { get; set; }
-        public int DuracaoTotal { get; set; }
+        // uso de lambda (ou no js == arrow function) onde retorno para a propriedade de "somente leitura" Duracao total a SOMA da duraćão de todas as músicas presentes na minha lista musicas (somente leitura pois quando eu uso uma lambda dessa forma, internamente, apenas o método GET é definido, logo, então, essa expressão lambda abaixo equivale ao novo GET da minha propriedade DuracaoTotal)\\
+        // Sum == método que realiza soma dos valores de uma lista onde seu primeiro parâmetro é uma funćão, que passamos uma lambda que percorre a lista que chamou o método Sum (== musicas), chamamos cada iteraćão em musicas de: musica e vamos realizando a soma do valor de cada música que passa para no fim retornarmos o valor total para DuracaoTotal
+        public int DuracaoTotal => musicas.Sum(musica => musica.Duracao);
 
         public void AdicionarMusica(Musica musica)
         {
             musicas.Add(musica);
         }
 
+        public void ExibirMusicasDoAlbum()
+        {
+            System.Console.WriteLine($"Lista de músicas do álbum {Nome}:");
+            foreach (var musica in musicas)
+            {
+                System.Console.WriteLine($"Música: {musica.Nome}");
+            }
+            System.Console.WriteLine($"Para ouvir este álbum ianteiro você precisa de {DuracaoTotal} segundos.");
+        }
     }
 }
--- aplicando-poo/ScreenSound/Program.cs
+++ aplicando-poo/ScreenSound/Program.cs
@@ -6,14 +6,16 @@ albumDoQueen.Nome = "A night at the opera";
 Musica musica1 = new Musica();
 musica1.Nome = "Love of my life";
 musica1.Artista = "Queen";
-musica1.Duracao = 10;
+musica1.Duracao = 213;
 musica1.Disponivel = true;
 
 Musica musica2 = new Musica();
 musica2.Nome = "Bohemian Rhapsody";
 musica2.Artista = "Queen";
-musica2.Duracao = 10;
+musica2.Duracao = 354;
 musica2.Disponivel = true;
 
 albumDoQueen.AdicionarMusica(musica1);
 albumDoQueen.AdicionarMusica(musica2);
+
+albumDoQueen.ExibirMusicasDoAlbum();
