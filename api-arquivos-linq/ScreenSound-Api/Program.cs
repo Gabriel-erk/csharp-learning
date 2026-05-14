648d28ca8484846a03c60f03f3ca049bf500d10b
Gabriel Erick <gabrielmmve13@gmail.com>
2026-05-14 17:59:21 -0300

    try catch

--- api-arquivos-linq/ScreenSound-Api/Program.cs
+++ api-arquivos-linq/ScreenSound-Api/Program.cs
@@ -1,10 +1,31 @@
 ﻿// neste projeto faremos requisićões http, logo, usaremos uma biblioteca (HttpClient) para nos auxiliar neste processo, onde a váriavel: client é o nome que damos a instância do objeto tipo HttpClient e quando atribuimos = new HttpClient() estamos INICIALIZANDO um objeto do tipo HTTPCLIENT)
 // como após o USING não fechamos as chaves, quer dizer que vamos gerenciar esse pacote/recurso dentro das {}
-using (HttpClient client = new HttpClient())
+// using (HttpClient client = new HttpClient())
+// {
+//     try
+//     {
+//         // tipo string pois a resposta que teremos provavelmente será do tipo string
+//         // getStringAsync pois NÃO sabemos quantas músicas tem, como é um método asincrono, ele não vai simplesmente pegar uma música e passar para a póxima, claro Do endpoint da api passada por parâmetro nos parenteses (endpoint == link/url), e claro como é um método ASSINCRONO, ele precisa do await, que basicamente diz para a máquina: espere essa atividade (retornos do endpoint da api) serem finalizados antes de prosseguir para a próxima linha
+//         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+//         System.Console.WriteLine(resposta);
+//     }
+//     catch (Exception e)
+//     {
+//         // Message == $th->getMessage, Message pega a mensagem que o objeto do tipo Exception pode ter
+//         System.Console.WriteLine($"Temos um problema: {e.Message}");
+//     }
+
+// }
+
+try
 {
-    // tipo string pois a resposta que teremos provavelmente será do tipo string
-    // getStringAsync pois NÃO sabemos quantas músicas tem, como é um método asincrono, ele não vai simplesmente pegar uma música e passar para a póxima, claro Do endpoint da api passada por parâmetro nos parenteses (endpoint == link/url), e claro como é um método ASSINCRONO, ele precisa do await, que basicamente diz para a máquina: espere essa atividade (retornos do endpoint da api) serem finalizados antes de prosseguir para a próxima linha
-    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-    System.Console.WriteLine(resposta);
+    for (int i = 3; i >= 0; i--)
+    {
+        Console.WriteLine($"{ 10/i}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Tivemos um problema: {ex.Message}");
 }
 
--- aplicando-poo/ScreenSound/obj/Debug/net10.0/ScreenSound.AssemblyInfo.cs
+++ aplicando-poo/ScreenSound/obj/Debug/net10.0/ScreenSound.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84de180e16518b759a821b847aec7069126ea24e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0fe1576f6addfd5e3cdbf16ee2237b8431949c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
